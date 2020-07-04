107239879c5d5173946bc6f8a38b57f0848398f1
Tobias Kunze <r@rixx.de>
2020-07-05 00:42:33 +0200

    Release v1.1.0

--- pretalx_pages/__init__.py
+++ pretalx_pages/__init__.py
@@ -13,7 +13,7 @@ class PluginApp(AppConfig):
             "Add static pages to your event site, for example Terms of Service, venue listings, a code of conduct, etc."
         )
         visible = True
-        version = "1.0.0"
+        version = "1.1.0"
 
     def ready(self):
         from . import signals  # NOQA
